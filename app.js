e41a4a4c7893ed5d968a4700e1cfa4f65413ea49
IsaacYJZhao <117480666+IsaacYJZhao@users.noreply.github.com>
2025-05-13 18:01:52 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 App({
   onLaunch() {
-    // 小程序启动时初始化
+    // 检查兼容性
+    if (wx.canIUse('onAccelerometerChange')) {
+      console.log('支持加速度计');
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用摇一摇功能',
+        showCancel: false
+      });
+    }
   },
   globalData: {
-    guaResult: null // 存储卦象结果
+    guaResult: null
   }
 })
